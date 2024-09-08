5cf634739422feab53c480d16568ba6e489b6a3a
Girish kumar <drdogks@gmail.com>
2024-09-08 12:45:01 +0530

    Create logoicons.js

---
+++ Components/LandingPage/Logo/logoicons.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import PocLogo from './logo.png'
+
+
+function logoicons() {
+  return (
+    <div>
+      <img  src={PocLogo} width={40} height={40} alt="POC"/>
+    </div>
+  )
+}
+
+export default logoicons
